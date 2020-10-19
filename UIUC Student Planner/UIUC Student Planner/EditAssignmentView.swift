12345c91160fb391bb16bff5afbcf065244b9533
Meghan Reynolds <meghanr6@illinois.edu>
2020-10-19 11:18:56 -0500

    <feat> added points field to editAssignmentView as well as save and cancel buttons

--- UIUC Student Planner/UIUC Student Planner/EditAssignmentView.swift
+++ UIUC Student Planner/UIUC Student Planner/EditAssignmentView.swift
@@ -14,8 +14,48 @@ struct EditAssignmentView: View {
     
     //The item passed in from the parent view
     @State var item: FetchedResults<Assignment>.Element
+    @State var newName: String = ""
+    @State var newPoints: Int64 = 0
     var body: some View {
-        Text("Hello World")
+        NavigationView {
+            Form {
+                Section(header: Text("Assignment Details")) {
+                    Stepper(value: $newPoints,in: 0...100){
+                       Text(getPoints())
+                    }
+                }
+                Button(action: {
+                    //update assignment
+                }, label : {
+                    HStack {
+                        Spacer()
+                        Text("Save")
+                        Spacer()
+                    }
+                })
+            }
+            .navigationBarTitle("Edit Assignment")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        //change view back to home view
+                    }, label : {
+                        Text("Cancel")
+                    })
+                }
+            }
+        }
+    }
+    func saveContext() {
+      do {
+        try viewContext.save()
+      } catch {
+        print("Error saving managed object context: \(error)")
+      }
+    }
+    func getPoints() -> String {
+        item.points = newPoints
+        return "\(item.points) Point\(item.points != 1 ? "s" : "")"
     }
 }
 
