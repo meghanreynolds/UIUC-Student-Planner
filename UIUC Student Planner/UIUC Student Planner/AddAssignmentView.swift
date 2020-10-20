10a5df717ed6bd6dad9cf3f1851dd6d7896399ff
Jeffrey Wang <jeffreywangcf@gmail.com>
2020-10-20 17:35:50 -0500

    <fix> abstraction of the due date picker view

--- UIUC Student Planner/UIUC Student Planner/AddAssignmentView.swift
+++ UIUC Student Planner/UIUC Student Planner/AddAssignmentView.swift
@@ -17,9 +17,6 @@ struct AddAssignmentView: View {
     @State var pointValue: Int64 = 0
     @State var selectedDate = Date()
     
-    @State var showSelectDateDetail = false
-    @State var showSelectTimeDetail = false
-    
     
     var body: some View {
         NavigationView {
@@ -32,7 +29,7 @@ struct AddAssignmentView: View {
                         Text("\(pointValue) Point\(pointValue != 1 ? "s" : "")")
                     }
                 }
-                self.deadline
+                DeadlineView(selectedDate: self.$selectedDate)
                 Button(action: {
                     addAssignment(name: assignmentName, points: pointValue, date: selectedDate)
                     //change view back to home view
@@ -59,62 +56,6 @@ struct AddAssignmentView: View {
                         Text("Cancel")
                     })
                 }
-            }
-        }
-    }
-    
-    var deadline: some View{
-        Section(header: Text("DEADLINE")){
-            HStack{
-                Image.init(systemName: "calendar")
-                    .font(.system(size: 18))
-                    .foregroundColor(.white).padding(5)
-                    .background(RoundedRectangle(cornerRadius: 7)
-                        .aspectRatio(1, contentMode: .fill)
-                        .foregroundColor(.red))
-                Text("Date")
-                    .padding(.leading, 5)
-                    .font(.system(size: 18))
-                Spacer()
-                Text("\(self.selectedDate.toString(dateStyle: .medium, timeStyle: .none))")
-                    .padding(.vertical, 10)
-            }
-            .contentShape(Rectangle())
-            .onTapGesture {
-                self.showSelectDateDetail.toggle()
-                self.showSelectTimeDetail = false
-            }
-            if self.showSelectDateDetail{
-                DatePicker.init(selection: self.$selectedDate, displayedComponents: .date){
-                    
-                }
-                .datePickerStyle(GraphicalDatePickerStyle.init())
-            }
-            
-            HStack{
-                Image.init(systemName: "clock.fill")
-                    .font(.system(size: 18))
-                    .foregroundColor(.white).padding(5)
-                    .background(RoundedRectangle(cornerRadius: 7)
-                        .aspectRatio(1, contentMode: .fill)
-                        .foregroundColor(.blue))
-                Text("Time")
-                    .padding(.leading, 5)
-                    .font(.system(size: 18))
-                Spacer()
-                Text("\(self.selectedDate.toString(dateStyle: .none, timeStyle: .short))")
-                .padding(.vertical, 10)
-            }
-            .contentShape(Rectangle())
-            .onTapGesture {
-                self.showSelectDateDetail = false
-                self.showSelectTimeDetail.toggle()
-            }
-            if self.showSelectTimeDetail{
-                DatePicker.init(selection: self.$selectedDate, displayedComponents: .hourAndMinute){
-                    
-                }
-                .datePickerStyle(GraphicalDatePickerStyle.init())
             }
         }
     }
